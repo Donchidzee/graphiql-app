74125728b762aa280ed7f63de066d4d2c8b86919
Donchidzee <donchik020202@gmail.com>
2024-09-15 16:31:00 +0200

    feat: update middleware

--- middleware.ts
+++ middleware.ts
@@ -13,22 +13,30 @@ export const config = {
 };
 
 export function middleware(request: NextRequest) {
-  const i18nResponse = i18nMiddleware(request);
-
   const url = new URL(request.url);
+  const token = request.cookies.get('token')?.value;
+
+  if (token) {
+    if (
+      url.pathname.includes('/login') ||
+      url.pathname.includes('/register') ||
+      url.pathname.includes('/reset')
+    ) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  } else {
+    if (url.pathname.includes('/api')) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
+
   if (
     url.pathname.includes('/login') ||
     url.pathname.includes('/register') ||
     url.pathname.includes('/reset')
   ) {
-    return i18nResponse || NextResponse.next();
+    return i18nMiddleware(request) || NextResponse.next();
   }
 
-  const token = request.cookies.get('token')?.value;
-
-  if (!token && url.pathname.includes('/api')) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-
-  return i18nResponse || NextResponse.next();
+  return i18nMiddleware(request) || NextResponse.next();
 }
